a02b45a5a93508c5c8a54b382bc927e238c8b66f
Marc Puig <marc.puig@gmail.com>
2023-04-08 10:57:14 +0200

    Initial color version

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,66 @@
-use image::{DynamicImage, ImageBuffer, Luma, GrayImage};
-use imageproc::filter::{gaussian_blur_f32};
-use imageproc::edges;
+use std::collections::VecDeque;
 use std::fs::File;
 use std::io::Write;
-use svg::Document;
-use svg::node::element::path::Data;
-use svg::node::element::{Path as SvgPath, Rectangle};
 
+use exoquant::{Color, convert_to_indexed, ditherer, optimizer};
+use image::{DynamicImage, GenericImageView, GrayImage, ImageBuffer, Luma, Pixel, Rgb, RgbImage};
+use imageproc::edges::canny;
+use imageproc::filter::gaussian_blur_f32;
+use svg::Document;
+use svg::node::element::{Path as SvgPath, Rectangle};
+use svg::node::element::path::Data;
 
 fn main() {
     println!("Read the input bitmap image.");
-    let input_image = image::open("input.png").unwrap();
+    let color_image = image::open("input.png").unwrap();
+
+    println!("Quantize the image using k-means clustering");
+    let num_colors = 16; // Adjust this value based on your requirements
+    // let quantized_image = quantize_colors(&color_image.to_rgb8(), num_colors);
+    let quantized_image = encode(&color_image, num_colors);
 
     println!("Preprocess the image (optional)");
-    let preprocessed_image = preprocess_image(&input_image);
+    let preprocessed_image = preprocess_image(&color_image);
 
     println!("Apply the tracing algorithm");
-    let vector_data = trace_bitmap(&preprocessed_image);
+    let vector_data = trace_bitmap(&preprocessed_image, &quantized_image);
 
     println!("Export the vector data to a file format (e.g., SVG)");
-    match export_vector_data(&vector_data, "output.svg", input_image.width(), input_image.height()) {
+    match export_vector_data(&vector_data, "output.svg", color_image.width(), color_image.height()) {
         Ok(_) => println!("Vector data successfully exported."),
         Err(e) => eprintln!("Failed to export vector data: {}", e),
     }
+}
+
+fn encode(imag: &image::DynamicImage, num_colors: usize) -> ImageBuffer<Rgb<u8>, Vec<u8>> {
+    let pixels = imag
+        .pixels()
+        .map(|(_, _, p)| {
+            let cols = p.channels();
+            Color::new(cols[0], cols[1], cols[2], cols[3])
+        })
+        .collect::<Vec<_>>();
+    let width = imag.width() as usize;
+    let height = imag.height() as usize;
+    let (_, indexed_pixels) = convert_to_indexed(
+        &pixels,
+        width,
+        num_colors,
+        &optimizer::KMeans,
+        &ditherer::FloydSteinberg::checkered(),
+    );
+
+    // Allocate a new buffer for the RGB image, 3 bytes per pixel
+    let mut output_data = vec![0u8; width * height * 3];
+
+    let mut i = 0;
+    // Iterate through 4-byte chunks of the image data (RGBA bytes)
+    for chunk in indexed_pixels.chunks(4) {
+        // ... and copy each of them to output, leaving out the A byte
+        output_data[i..i + 3].copy_from_slice(&chunk[0..3]);
+        i += 3;
+    }
+    RgbImage::from_raw(width as u32, height as u32, output_data).unwrap()
 }
 
 fn preprocess_image(image: &DynamicImage) -> ImageBuffer<Luma<u8>, Vec<u8>> {
@@ -74,17 +112,18 @@ fn ramer_douglas_peucker(points: Vec<(f64, f64)>, epsilon: f64) -> Vec<(f64, f64
     result
 }
 
-fn trace_bitmap(image: &GrayImage) -> Vec<Path> {
+fn trace_bitmap(image: &ImageBuffer<Luma<u8>, Vec<u8>>, color_image: &RgbImage) -> Vec<Path> {
     println!("Apply the Canny edge detection algorithm");
     let low_threshold = 10.0;
     let high_threshold = 50.0;
-    let canny_image = edges::canny(image, low_threshold, high_threshold);
+    let edge_image = canny(image, low_threshold, high_threshold);
 
     println!("Extract contours from the edge-detected image");
-    let contours = extract_contours(&canny_image);
+    let contours = extract_contours(&edge_image, color_image);
+    println!(" -> Extracted contours: {}", contours.len());
 
     println!("Simplify the contours using the Ramer-Douglas-Peucker algorithm");
-    let epsilon = 0.1;
+    let epsilon = 1.0;
     let simplified_contours: Vec<Path> = contours
         .into_iter()
         .map(|path| {
@@ -96,7 +135,7 @@ fn trace_bitmap(image: &GrayImage) -> Vec<Path> {
 
             let simplified_points = ramer_douglas_peucker(points, epsilon);
 
-            let mut simplified_path = Path::new(Point::new(simplified_points[0].0, simplified_points[0].1));
+            let mut simplified_path = Path::new(Point::new(simplified_points[0].0, simplified_points[0].1), path.color);
             for point in simplified_points.iter().skip(1) {
                 simplified_path.line_to(Point::new(point.0, point.1));
             }
@@ -107,80 +146,59 @@ fn trace_bitmap(image: &GrayImage) -> Vec<Path> {
     simplified_contours
 }
 
-fn extract_contours(image: &GrayImage) -> Vec<Path> {
+fn extract_contours(edge_image: &GrayImage, color_image: &RgbImage) -> Vec<Path> {
     let mut contours = Vec::new();
-    let mut visited = vec![vec![false; image.height() as usize]; image.width() as usize];
+    let mut visited = vec![vec![false; edge_image.height() as usize]; edge_image.width() as usize];
 
-    for (x, y, pixel) in image.enumerate_pixels() {
+    for (x, y, pixel) in edge_image.enumerate_pixels() {
         if pixel == &Luma([255u8]) && !visited[x as usize][y as usize] {
-            let contour_points = moore_neighbor_tracing(image, &mut visited, x, y);
+            let contour_points = trace_contour(edge_image, color_image, &mut visited, x, y);
             if !contour_points.is_empty() {
-                let mut contour_path = Path::new(contour_points[0]);
-                for point in contour_points.iter().skip(1) {
-                    contour_path.line_to(*point);
+                let mut contour_path = Path::new(contour_points[0].0, contour_points[0].1);
+                for (point, color) in contour_points.iter().skip(1) {
+                    contour_path.line_to_with_color(*point, *color);
                 }
                 contours.push(contour_path);
             }
         }
     }
-
     contours
 }
 
-fn moore_neighbor_tracing(image: &GrayImage, visited: &mut Vec<Vec<bool>>, x: u32, y: u32) -> Vec<Point> {
+fn trace_contour(edge_image: &GrayImage, color_image: &RgbImage, visited: &mut Vec<Vec<bool>>, x: u32, y: u32) -> Vec<(Point, Rgb<u8>)> {
+    let width = edge_image.width();
+    let height = edge_image.height();
+
+    let is_inside = |x: i32, y: i32| x >= 0 && y >= 0 && x < width as i32 && y < height as i32;
+    let neighbors: [(i32, i32); 8] = [(-1, 0), (1, 0), (0, -1), (0, 1), (-1, -1), (-1, 1), (1, -1), (1, 1)];
+
     let mut contour_points = Vec::new();
-    let mut current_point = (x, y);
-    let mut backtrack_point = (0, 0);
-    let mut backtracked = false;
+    let mut stack = VecDeque::new();
+    stack.push_back((x as i32, y as i32));
 
-    let moore_neighbors = [
-        (-1, 0),
-        (-1, -1),
-        (0, -1),
-        (1, -1),
-        (1, 0),
-        (1, 1),
-        (0, 1),
-        (-1, 1),
-    ];
+    while let Some(current_point) = stack.pop_front() {
+        let x = current_point.0;
+        let y = current_point.1;
 
-    while !visited[current_point.0 as usize][current_point.1 as usize] || !backtracked {
-        visited[current_point.0 as usize][current_point.1 as usize] = true;
-        contour_points.push(Point::new(current_point.0 as f64, current_point.1 as f64));
+        if is_inside(x, y) && !visited[x as usize][y as usize] && edge_image.get_pixel(x as u32, y as u32).0[0] != 0 {
+            visited[x as usize][y as usize] = true;
+            contour_points.push((
+                Point::new(current_point.0 as f64, current_point.1 as f64),
+                color_image.get_pixel(current_point.0 as u32, current_point.1 as u32).clone(),
+            ));
+            for &(dx, dy) in &neighbors {
+                let new_x = x + dx;
+                let new_y = y + dy;
 
-        let start_index = if backtracked {
-            (backtrack_point.0 + 1) % 8
-        } else {
-            0
-        };
-
-        backtracked = false;
-
-        for i in start_index..8 {
-            let next_point = (
-                current_point.0 as i32 + moore_neighbors[i].0,
-                current_point.1 as i32 + moore_neighbors[i].1,
-            );
-
-            if next_point.0 >= 0
-                && next_point.1 >= 0
-                && next_point.0 < image.width() as i32
-                && next_point.1 < image.height() as i32
-                && image.get_pixel(next_point.0 as u32, next_point.1 as u32) == &Luma([255u8])
-                && !visited[next_point.0 as usize][next_point.1 as usize]
-            {
-                backtrack_point = (i, i);
-                current_point = (next_point.0 as u32, next_point.1 as u32);
-                break;
-            } else if i == 7 {
-                backtracked = true;
-                backtrack_point = (backtrack_point.1, (backtrack_point.1 + 1) % 8);
+                if is_inside(new_x, new_y) && !visited[new_x as usize][new_y as usize] {
+                    stack.push_back((new_x, new_y));
+                }
             }
         }
     }
-
     contour_points
 }
+
 
 fn export_vector_data(paths: &[Path], file_name: &str, width: u32, height: u32) -> Result<(), Box<dyn std::error::Error>> {
     let mut document = Document::new()
@@ -201,7 +219,7 @@ fn export_vector_data(paths: &[Path], file_name: &str, width: u32, height: u32) 
 
         for segment in &path.segments {
             match segment {
-                Segment::Line(end) => {
+                Segment::Line(end, ..) => {
                     data = data.line_to((end.x, end.y));
                 }
                 Segment::QuadraticBezier(control, end) => {
@@ -241,7 +259,7 @@ impl Point {
 
 // Next, we'll create a Segment enum to represent line segments and quadratic Bezier curves:
 pub enum Segment {
-    Line(Point),
+    Line(Point, Rgb<u8>),
     QuadraticBezier(Point, Point),
 }
 
@@ -250,19 +268,25 @@ pub enum Segment {
 // The Path struct could be represented by a collection of points and Bezier curves
 pub struct Path {
     pub start: Point,
+    pub color: Rgb<u8>,
     pub segments: Vec<Segment>,
 }
 
 impl Path {
-    pub fn new(start: Point) -> Self {
+    pub fn new(start: Point, color: Rgb<u8>) -> Self {
         Self {
             start,
+            color,
             segments: Vec::new(),
         }
     }
 
     pub fn line_to(&mut self, end: Point) {
-        self.segments.push(Segment::Line(end));
+        self.segments.push(Segment::Line(end, self.color));
+    }
+
+    pub fn line_to_with_color(&mut self, end: Point, color: Rgb<u8>) {
+        self.segments.push(Segment::Line(end, color));
     }
 
     pub fn quadratic_bezier_to(&mut self, control: Point, end: Point) {
@@ -273,7 +297,7 @@ impl Path {
         let mut points = vec![self.start];
         for segment in &self.segments {
             match segment {
-                Segment::Line(end) => {
+                Segment::Line(end, ..) => {
                     points.push(*end);
                 }
                 Segment::QuadraticBezier(control, end) => {
